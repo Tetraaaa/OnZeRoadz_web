ae67b76884338ccc59d2ffcaa54c80f15cc08d75
Fatih Sarica <fatihsarica67@gmail.com>
2019-02-07 17:27:44 +0100

    Ajout de la recherche de lieu à partir de la saisie

--- georallye/src/components/Home.js
+++ georallye/src/components/Home.js
@@ -1,23 +1,66 @@
 import React, { Component } from 'react';
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
-import { Row, Col, FormControl, Button, InputGroup, Prepend, Text } from 'react-bootstrap';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { Row, Col, FormControl, Button, ListGroup, ListGroupItem } from 'react-bootstrap';
 import '../styles/Home.css';
 import '../App.css';
 
 class Home extends Component {
     state = {
         lat: 0,
-        long: 0
+        lng: 0,
+        markers: null,
+        predictions: [],
+        lieu: ''
     }
+
+    requests = [];
+
+    handleInputChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
     location = () => {
         navigator.geolocation.getCurrentPosition((position) => {
             this.setState({
                 lat: position.coords.latitude,
-                long: position.coords.longitude
+                lng: position.coords.longitude
             })
         })
     }
+
+    mapClicked = (mapProps, map, clickEvent) => {
+        this.setState({
+            markers: {
+                lat: clickEvent.latLng.lat(),
+                lng: clickEvent.latLng.lng()
+            }
+        })
+    }
+
+    _onSearch = (item) => {
+        console.log(item)
+        fetch("https://maps.googleapis.com/maps/api/place/details/json?&placeid=" +
+            item.place_id + "&key=AIzaSyAJiED9aRjJTSCUHmBE2pUZg4OifcAenpk").then(response => {
+                if (response.ok) {
+                    response.json().then(json => this.setState({ lat: json.result.geometry.location.lat, lng: json.result.geometry.location.lng, lieu: item.description }))
+                }
+            })
+            .catch(error => console.log(error))
+    }
+
+    _onChangeText = (e) => {
+        this.requests.forEach(request => request.abort())
+        if (e.target.value.length > 2) {
+            fetch("https://maps.googleapis.com/maps/api/place/autocomplete/json?&input=" +
+                e.target.value + "&key=AIzaSyAJiED9aRjJTSCUHmBE2pUZg4OifcAenpk").then(response => {
+                    if (response.ok) {
+                        response.json().then(json => this.setState({ predictions: json.predictions }))
+                    }
+                })
+                .catch(error => console.log(error))
+        }
+        this.handleInputChange(e);
+    }
+
+
 
     componentDidMount() {
         this.location()
@@ -30,7 +73,22 @@ class Home extends Component {
                     <Col xs={7}>
                         <Row>
                             <Col xs={7}>
-                                <FormControl className="input-lieu" type="text" placeholder="Rechercher un lieu" />
+                                <FormControl className="input-lieu"
+                                    type="text"
+                                    name="lieu"
+                                    value={this.state.lieu}
+                                    placeholder="Rechercher un lieu"
+                                    onChange={this._onChangeText} />
+                                <ListGroup className="list-lieu">
+                                    {
+                                        this.state.predictions.map((item) => {
+                                            return (
+                                                <ListGroupItem onClick={() => { this._onSearch(item) }}>{item.description}</ListGroupItem>
+                                            )
+                                        }
+                                        )
+                                    }
+                                </ListGroup>
                             </Col>
                             <Col xs={5}>
                                 <Button>Filtrer la recherche</Button>
@@ -38,7 +96,17 @@ class Home extends Component {
                         </Row>
                         <Row>
                             <Col className="map-container">
-                                <Map classname="map" google={this.props.google} center={{ lat: this.state.lat, lng: this.state.long }} zoom={14}></Map>
+                                <Map className="map" google={this.props.google} center={{ lat: this.state.lat, lng: this.state.lng }} zoom={14} onClick={this.mapClicked}>
+                                    <Marker position={{ lat: this.state.lat, lng: this.state.lng }} />
+                                    {this.state.markers ?
+                                        <Marker position={{ lat: this.state.markers.lat, lng: this.state.markers.lng }} />
+                                        :
+                                        null
+                                    }
+                                    <Button className="btn-mylocation" onClick={this.location}>
+                                        <i className="material-icons">my_location</i>
+                                    </Button>
+                                </Map>
                             </Col>
                         </Row>
                     </Col>
