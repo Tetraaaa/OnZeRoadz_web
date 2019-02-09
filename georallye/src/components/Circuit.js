29017e677e0a6454da1ee1f6402e241425ae9b5e
Fatih Sarica <fatihsarica67@gmail.com>
2019-02-09 18:39:37 +0100

    Test react-sidebar

--- georallye/src/components/Circuit.js
+++ georallye/src/components/Circuit.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react'
 import '../App.css';
+import Sidebar from "react-sidebar";
 
 class Circuit extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+          sidebarOpen: false
+        };
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+      }
+    
+      onSetSidebarOpen() {
+        if(this.state.sidebarOpen===false)
+            this.setState({ sidebarOpen: true })
+        else
+            (this.setState({ sidebarOpen: false }));
+      }
+    
+      render() {
         return (
-            <div></div>
-        )
+          <Sidebar
+            sidebar={<b>Sidebar content</b>}
+            docked={this.state.sidebarOpen}
+            onSetOpen={this.onSetSidebarOpen}
+            styles={{ sidebar: { background: "white" } }}
+            pullRight
+          >
+            <button onClick={() => this.onSetSidebarOpen()}>
+              Open sidebar
+            </button>
+          </Sidebar>
+        );
+      }
     }
-}
 
 export default Circuit;
